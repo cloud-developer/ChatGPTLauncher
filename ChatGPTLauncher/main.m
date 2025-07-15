152afdd9a5842443c1c3394ad9333c4848761fa3
cLoud <cloud-developer@users.noreply.github.com>
2025-07-15 19:33:26 -0400

    Update main.m to remove AppDelegate

--- ChatGPTLauncher/main.m
+++ ChatGPTLauncher/main.m
@@ -4,17 +4,6 @@
 @property (strong, nonatomic) UIWindow *window;
 @end
 
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSURL *url = [NSURL URLWithString:@"https://chatgpt.com"];
-    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
-    exit(0); // Exit immediately after opening browser
-    return YES;
-}
-
-@end
-
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
